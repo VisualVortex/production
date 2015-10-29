171eb2344716a99e06c48b2f1db94041a983aa40
VisualVortex <toolive@list.ru>
2015-10-30 02:58:39 +0300

    Close #11 - нужно сделать пул реквест

--- frontend/components/editArea/index.js
+++ frontend/components/editArea/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import MessageActions from '../../actions/message';
 import UserActions from '../../actions/usersList.js';
 import TextareaSize from 'react-textarea-autosize';
-const ENTER_KEY_CODE = 13; // код клавиши enter
-const ESC_KEY_CODE = 27; // код клавиши esc
+const ENTER_KEY_CODE = 13;
+const ESC_KEY_CODE = 27;
 
 export default class EditArea extends React.Component {
 
